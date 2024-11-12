699078a8ffd2ff536ea1c2300580ccadc6966c1f
Matheus Rodrigues Tomaz <157989074+matheusrtomaz@users.noreply.github.com>
2024-11-12 15:00:25 -0300

    deleteInvoice

--- nextjs-dashboard/app/ui/invoices/buttons.tsx
+++ nextjs-dashboard/app/ui/invoices/buttons.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
+
   return (
     <Link
       href="/dashboard/invoices/create"
@@ -15,6 +16,7 @@ export function CreateInvoice() {
 }
 
 export function UpdateInvoice({ id }: { id: string }) {
+
   return (
     <Link
       href={`/dashboard/invoices/${id}/edit`}
@@ -26,12 +28,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <form action={deleteInvoiceWithId}>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
+        <TrashIcon className="w-4" />
       </button>
-    </>
+    </form>
   );
 }
